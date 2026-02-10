f3aacafaf7111025897a9845cefe37334f1e682a
Adele <mcshanealincoln@gmail.com>
2026-02-09 21:55:17 -0500

    most recent autos

--- src/main/java/frc/robot/subsystems/spindexer/SpindexerIOSim.java
+++ src/main/java/frc/robot/subsystems/spindexer/SpindexerIOSim.java
@@ -6,23 +6,23 @@ import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 
 public class SpindexerIOSim implements SpindexerIO {
-    private FlywheelSim sim = new FlywheelSim(
-        LinearSystemId.createFlywheelSystem(DCMotor.getNEO(1), .004, 1.0), 
-        DCMotor.getNEO(1));
-    private double appliedVolts = 0.0;
+  private FlywheelSim sim =
+      new FlywheelSim(
+          LinearSystemId.createFlywheelSystem(DCMotor.getNEO(1), .004, 1.0), DCMotor.getNEO(1));
+  private double appliedVolts = 0.0;
 
-    @Override
-    public void updateInputs(SpindexerIOInputs inputs) {
-        sim.update(0.020); // Move physics forward 20ms
+  @Override
+  public void updateInputs(SpindexerIOInputs inputs) {
+    sim.update(0.020); // Move physics forward 20ms
 
-        inputs.velocityRps = sim.getAngularVelocityRPM() / 60.0;
-        inputs.appliedVolts = appliedVolts;
-        inputs.currentAmps = new double[] { sim.getCurrentDrawAmps() };
-    }
+    inputs.velocityRps = sim.getAngularVelocityRPM() / 60.0;
+    inputs.appliedVolts = appliedVolts;
+    inputs.currentAmps = new double[] {sim.getCurrentDrawAmps()};
+  }
 
-    @Override
-    public void setVoltage(double volts) {
-        appliedVolts = MathUtil.clamp(volts, -12.0, 12.0);
-        sim.setInputVoltage(appliedVolts);
-    }
+  @Override
+  public void setVoltage(double volts) {
+    appliedVolts = MathUtil.clamp(volts, -12.0, 12.0);
+    sim.setInputVoltage(appliedVolts);
+  }
 }
